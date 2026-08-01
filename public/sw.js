269a2adad6daa5ebe286bc6d9b6ab856dcc3eefb
Eiliya <eiliya.abedini@gmail.com>
2026-08-01 15:13:29 +0200

    fix the loop

--- public/sw.js
+++ public/sw.js
@@ -19,6 +19,8 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+  const url = new URL(event.request.url);
+  if (url.pathname.startsWith("/_next") || url.pathname.includes("_turbopack")) return;
   event.respondWith(
     fetch(event.request)
       .then((response) => {
